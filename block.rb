4116e73e83b880252e5b8f0c141b7b3980b354bd
Tyler Church <tylertrain@yahoo.com>
2009-01-30 16:09:47 -0700

    Added barebones file

---
+++ block.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'gosu'
+
+class GameWindow < Gosu::Window
+	def initialize
+		super 640, 480, false
+		self.caption = "Block Bounce!"
+	end
+	
+	def update
+	end
+	
+	def draw
+	end
+	
+	def button_down id
+		if id == Gosu::Button::KbEscape
+			close
+		end
+	end
+end
+
+module ZOrder
+	Background, Objects = *0..1
+end
+
+window = GameWindow.new
+window.show
