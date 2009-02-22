36d9fef10a3c9abc79919eb136016c726ef0534d
Tyler Church <tylertrain@yahoo.com>
2009-02-21 22:38:49 -0700

    We now draw from the @tiles hash. Commands can be entered in the terminal when "e" is pressed.

--- block.rb
+++ block.rb
@@ -15,8 +15,16 @@ class TileManager
 		@tile_size_y = 32
 		#@font = Rubygame::TTF.new 'FreeSans.ttf', @tile_size/2
 		@selx, @sely = 0, 0
-		
-		
+		@tiles = {}
+		(@screen.width/@tile_size_x).times do |x|
+			(@screen.height/@tile_size_y).times do |y|
+				@tiles[[x,y]] = nil
+			end
+		end
+		#puts @tiles.inspect.sort
+		puts @tiles.length
+		puts @tiles[[0,0]]
+		puts @tiles[[19,19]]
 	end
 	
 	def run
@@ -41,13 +49,23 @@ class TileManager
 								@selx += 1
 							when Rubygame::K_SPACE
 								puts @selx.to_s + '.' + @sely.to_s
+							when Rubygame::K_E
+								begin
+									eval gets.chomp
+								rescue
+									puts 'ERROR!'
+								end
 						end
 				end
 			end
 			# Draw the tile grid
-			(@screen.width/@tile_size_x).times do |x|us
+			(@screen.width/@tile_size_x).times do |x|
 				(@screen.height/@tile_size_y).times do |y|
 					@screen.draw_box [x * @tile_size_x, y * @tile_size_y], [x * @tile_size_x + @tile_size_x, y * @tile_size_y + @tile_size_y] , [0,255,0]
+					begin
+						@tiles[[x,y]].blit @screen, [x * @tile_size_x, y * @tile_size_y]
+					rescue NoMethodError
+					end
 					#@font.render("#{x.to_s}.#{y.to_s}", true, [255,0,0]).blit(@screen,[x * @tile_size, y * @tile_size])
 				end
 			end
