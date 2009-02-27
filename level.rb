1f25ed1ab443477367b77ea281c6da3072dd0dd5
Tyler Church <tylertrain@yahoo.com>
2009-02-26 22:12:37 -0700

    Started fleshing out the level editor

---
+++ level.rb
@@ -0,0 +1,31 @@
+require 'rubygame'
+require 'yaml'
+Rubygame::TTF.setup
+
+class LevelEditor
+	def intialize
+		@screen = Rubygame::Screen.new [640,640], 0, [Rubygame::HWSURFACE, Rubygame::DOUBLEBUF]
+		@screen.title = "Level Editor"
+		
+		@queue = Rubygame::EventQueue.new
+		@clock = Rubygame::Clock.new
+		@clock.target_framerate = 30
+	end
+	
+	def run
+		loop do
+			update
+			draw
+			@clock.tick
+		end
+	end
+	
+	def update
+	end
+	
+	def draw
+	end
+end
+
+leveler = LevelEditor.new
+leveler.run
