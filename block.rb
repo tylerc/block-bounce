1b50a0b6f0d3538985be4de15b52e898db7faa9b
Tyler Church <tylertrain@yahoo.com>
2009-03-15 18:00:48 -0600

    Fleshed out the basis of Block Bounce

--- block.rb
+++ block.rb
@@ -1,1 +1,29 @@
-puts "Block Bounce isn't written yet"
+require 'rubygame'
+
+class Game
+	def initialize
+		@screen = Rubygame::Screen.new [512,544], 0, [Rubygame::HWSURFACE, Rubygame::DOUBLEBUF]
+		@screen.title = "Block Bounce!"
+		
+		@queue = Rubygame::EventQueue.new
+		@clock = Rubygame::Clock.new
+		@clock.target_framerate = 30
+	end
+	
+	def run
+		loop do
+			update
+			draw
+			@clock.tick
+		end
+	end
+	
+	def update
+	end
+	
+	def draw
+	end
+end
+
+block_bounce = Game.new
+block_bounce.run
