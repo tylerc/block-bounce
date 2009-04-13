c326f15300bf4f5e0ca48181973b3fdd4747f712
Tyler Church <tylertrain@yahoo.com>
2009-04-12 19:58:57 -0700

    added a level load box

--- properties.rb
+++ properties.rb
@@ -11,9 +11,20 @@ window.signal_connect("destroy") {
   Gtk.main_quit
 }
 
+table = Gtk::Table.new(1,2,true)
+
+hbox = Gtk::HBox.new(false,5)
+level = Gtk::Entry.new
+level_label = Gtk::Label.new("Level:")
+hbox.pack_start_defaults(level_label)
+hbox.pack_start_defaults(level)
+
+table.attach(hbox,0,1,0,1)
+
 window.border_width = 5
 window.set_size_request(500,300)
 window.title = "Level Properties Editor"
+window.add(table)
 window.show_all
 
 Gtk.main
