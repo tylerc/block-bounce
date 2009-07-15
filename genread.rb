f370a51c4caa2475a3c17c85b1753b65d4e74376
Tyler Church <tylertrain@yahoo.com>
2009-07-15 01:27:16 -0700

    README is now valid XHTML strict

--- genread.rb
+++ genread.rb
@@ -8,6 +8,10 @@ input.close
 output = File.new "README.html", "w"
 output.puts <<HERE
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+<title>Block Bounce README</title>
 <style type="text/css">
 h2.author
 {
@@ -48,6 +52,12 @@ background: #DDD;
 padding: 5px;
 }
 </style>
+</head>
+<body>
 HERE
 output.puts RedCloth.new(text).to_html
+output.puts <<HERE
+</body>
+</html>
+HERE
 output.close
