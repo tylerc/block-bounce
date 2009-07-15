eef6db29a2244492b1e040eeff91c5ecc69db849
Tyler Church <tylertrain@yahoo.com>
2009-07-15 00:52:55 -0700

    Updates to README (Especially README.html)

--- genread.rb
+++ genread.rb
@@ -6,5 +6,48 @@ text = input.readlines.join
 input.close
 
 output = File.new "README.html", "w"
+output.puts <<HERE
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<style type="text/css">
+h2.author
+{
+font-size: 15pt;
+}
+h2.version
+{
+font-size: 14pt;
+}
+div.heading, div.content { margin-top: 35px; margin-left: 35px; }
+div.content
+{
+width: 30%;
+background: #FFD;
+padding: 30px;
+float: left;
+}
+div.title
+{
+padding: 30px;
+background: #DFF;
+}
+div.heading
+{
+width: 350px;
+float: left;
+}
+div.license
+{
+margin-top: 35px;
+padding: 30px;
+background: #FDF;
+}
+code
+{
+font-size: 10pt;
+background: #DDD;
+padding: 5px;
+}
+</style>
+HERE
 output.puts RedCloth.new(text).to_html
 output.close
